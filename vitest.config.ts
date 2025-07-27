72065a6866a1ee5f9572ad6eccf7cd49013c212c
Kazki Ueda <themeaningof8@gmail.com>
2025-07-27 12:11:22 +0900

    vitest.config.tsにvitestの設定を含めるためのコメントを追加しました。

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  // @ts-expect-error: vitestの設定をviteのconfigに含めるため
   test: {
     globals: true,
     environment: 'jsdom',
